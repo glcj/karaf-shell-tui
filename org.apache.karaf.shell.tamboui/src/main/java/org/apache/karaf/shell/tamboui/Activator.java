b7ee8eac7104854a7ed1c36298d69a0d44cbc163
César García <cesar.garcia@ceos.com.ve>
2026-06-13 16:39:01 -0400

    First release.

--- org.apache.karaf.shell.tamboui/src/main/java/org/apache/karaf/shell/tamboui/Activator.java
+++ org.apache.karaf.shell.tamboui/src/main/java/org/apache/karaf/shell/tamboui/Activator.java
@@ -21,10 +21,12 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+    @Override
     public void start(BundleContext context) {
 //        System.out.println("Starting the bundle");
     }
 
+    @Override
     public void stop(BundleContext context) {
 //        System.out.println("Stopping the bundle");
     }
